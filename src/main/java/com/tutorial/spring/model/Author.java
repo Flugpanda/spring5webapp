4a5f3f58871c2420e17294d2d405b13718e96e4f
Bastian Bräunel <bastian.braeunel@gmx.de>
2018-01-08 10:29:57 +0100

    Initial commit of the spring project
    	geändert:       .gitignore
    	neue Datei:     build.gradle

---
+++ src/main/java/com/tutorial/spring/model/Author.java
@@ -0,0 +1,123 @@
+package com.tutorial.spring.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+/**
+ * 
+ * @author santa
+ * A class that represents an author.
+ *
+ */
+@Entity
+public class Author {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String firstName;
+	private String lastName;
+
+	@ManyToMany(mappedBy="authors")
+	private Set<Book> books = new HashSet<Book>();
+
+	/**
+	 * default constructor
+	 */
+	public Author() {
+		firstName = "";
+		lastName = "";
+	}
+	
+	/**
+	 * Constructor that crates an author.
+	 * @param first	the first name of the author
+	 * @param last	the last name if the author
+	 */
+	public Author(String first, String last) {
+		firstName = first;
+		lastName = last;
+	}
+	
+	/**
+	 * 
+	 * Constructor that crates an author.
+	 * @param first	the first name of the author
+	 * @param last	the last name if the author
+	 * @param books	a set of book that are written by the author
+	 */
+	public Author(String first, String last, Set<Book> books) {
+		firstName = first;
+		lastName = last;
+		this.books = books;
+	}
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Set<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(Set<Book> books) {
+		this.books = books;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", books=" + books + "]";
+	}
+}
---
+++ src/main/java/com/tutorial/spring/model/Book.java
@@ -0,0 +1,147 @@
+package com.tutorial.spring.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+/**
+ * 
+ * @author santa
+ * A class that represents an book.
+ */
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String isbn;
+	private String title;
+	private String publischer;
+
+	/**
+	 * change hibernates default generated mapping
+	 * 
+	 * @JoinTable(name="author_book"			> creates a single join table called author_book
+	 * joinColumns=@JoinColumn(name="book_id")	> joining authors and books by the book_id
+	 * inverseJoinColumns=@JoinColumn(name="author_id")	> specifying the other direction of the join by the author_id
+	 */
+	@ManyToMany
+	@JoinTable(name="author_book", joinColumns=@JoinColumn(name="book_id"), inverseJoinColumns=@JoinColumn(name="author_id"))
+	private Set<Author> authors = new HashSet<Author>();
+	
+	/**
+	 * default constructor
+	 */
+	public Book() {
+		this.title = "no title";
+		this.isbn = "no isbn";
+		this.publischer = "unknown";
+	}
+	
+	/**
+	 * Constructor to create a book
+	 * @param title		the title of the book
+	 * @param isbn		the isbn number of the book
+	 * @param publisher	the publisher of the book
+	 */
+	public Book(String title, String isbn, String publisher) {
+		this.title = title;
+		this.isbn = isbn;
+		this.publischer = publisher;
+	}
+	
+	/**
+	 * 
+	 * Constructor to create a book
+	 * @param title		the title of the book
+	 * @param isbn		he isbn number of the book
+	 * @param publisher	the publisher of the book
+	 * @param authors	a set of authors that wrote this book
+	 */
+	public Book(String title, String isbn, String publisher, Set<Author> authors) {
+		this.title = title;
+		this.isbn = isbn;
+		this.publischer = publisher;
+		this.authors = authors;
+	}
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPublischer() {
+		return publischer;
+	}
+
+	public void setPublischer(String publischer) {
+		this.publischer = publischer;
+	}
+
+	public Set<Author> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(Set<Author> authors) {
+		this.authors = authors;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", isbn=" + isbn + ", title=" + title + ", publischer=" + publischer + ", authors="
+				+ authors + "]";
+	}	
+}
